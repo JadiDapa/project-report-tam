9c52db63e2799ef59beae0032901aad58e8110a3
Daffa Althaf Ridovani <daffaalthaf25@gmail.com>
2025-10-16 10:56:38 +0700

    chore: update dependencies and remove reset-project script
    - Updated various package dependencies in package.json to their latest versions, including @expo/vector-icons, react, react-native, and several expo packages.
    - Removed the reset-project.js script, which was used to reset the project structure, as it is no longer needed.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,20 @@
-module.exports = function(api) {
-    api.cache(true);
-
-    return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
-            root: ["./"],
-
-            alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }]]
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./",
+            "tailwind.config": "./tailwind.config.js",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin", // ⚡ MUST be last
+    ],
+  };
 };
